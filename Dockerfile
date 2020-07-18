0e7dab35f726af4e1599c950c7a3e720f3e23faa
Shubhang Tiwari <shubhang.tiwari@sap.com>
2020-07-19 00:32:08 +0530

    Correcting Files

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,9 @@
-FROM node:14.4.0-slim
+FROM sapui5:latest
 
 EXPOSE 80
 WORKDIR /ui5-trainer
-COPY . .
+COPY package.json .
+COPY app.js .
 
-ENV SDK_URL https://tools.hana.ondemand.com/additional/sapui5-sdk-1.79.2.zip
-
-RUN apt update && apt install wget unzip --yes
-RUN wget --header "Cookie: eula_3_1_agreed=tools.hana.ondemand.com/developer-license-3_1.txt;" -q -O ui5.zip $SDK_URL
-RUN mkdir /sap && unzip ui5.zip -d /sap/ui5 >/dev/null
-RUN rm ui5.zip
 RUN npm install
-
-CMD node index.js
+CMD node app.js
--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 // Fetching the libraries
 const express = require('express');
+const fs = require('fs');
 const port = process.port || 80;
 
 let app = express();
 
 // Serving the resources
-app.use('/', express.static('/source-code'));
+if (fs.existsSync('/source')) {
+    app.use('/', express.static('/source'));
+    app.use('/sample', express.static(__dirname + '/sample'));
+} else {
+    app.use('/', express.static(__dirname + '/sample'));
+}
 app.use('/ui5', express.static('/sap/ui5'));
 
 // Starting the server
